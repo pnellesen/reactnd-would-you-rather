02c9fe88f1b9b8cf9b74e1cab29903d841f42644
pnellesen <pnellesen@hotmail.com>
2018-07-24 11:00:00 -0500

    Added inital answer form to individual questions page

--- src/components/ViewQuestion.js
+++ src/components/ViewQuestion.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import { Button  } from 'reactstrap';
+
 
 class ViewQuestion extends Component {
+  state = {
+    answer: ''
+  };
+
+  _onSubmit = (evt) => {
+    evt.preventDefault();
+    console.log("submit - answser? ", this.state, " - props: ", this.props);
+  }
+
+  _handleAnswerChange = (evt) => {
+    this.setState({answer: evt.target.value})
+  }
   render() {
-    const { question_id, question, authedUser, gotQuestion, votes1, votes2, authorName, hasAnswered } = this.props
+    const { question_id, question, authedUser, votes1, votes2, authorName, hasAnswered } = this.props
+    const {answer} = this.state
     return (
-      (gotQuestion) ? 
+      (question) ?
       <div>
         <h1 style={{display: 'inline-block'}}>Would you rather..</h1>{authorName !== '' && <span> (Submitted by {authorName})</span>}
-        <div><span className={votes1.includes(authedUser) ? 'isBold' : ''}>{question.optionOne.text}</span> {hasAnswered && `Votes for: ${votes1.length}`} </div>
-        <div>or</div>
-        <div><span className={votes2.includes(authedUser) ? 'isBold' : ''}>{question.optionTwo.text}</span> {hasAnswered && `Votes for: ${votes2.length}`}</div>
+        <form name={'answerForm'} onSubmit={(e) => this._onSubmit(e)}>
+          <fieldset>
+          <div><span className={votes1.includes(authedUser) ? 'isBold' : ''}>{question.optionOne.text}</span> {hasAnswered ? `Votes for: ${votes1.length}` : <input type={'radio'} value={'optionOne'} onChange={(e) => this._handleAnswerChange(e)} checked={answer === 'optionOne'}/>} </div>
+
+          <div>or</div>
+
+          <div><span className={votes2.includes(authedUser) ? 'isBold' : ''}>{question.optionTwo.text}</span> {hasAnswered ? `Votes for: ${votes2.length}` : <input type={'radio'} value={'optionTwo'} onChange={(e) => this._handleAnswerChange(e)} checked={answer === 'optionTwo'}/>}</div>
+          </fieldset>
+          { !hasAnswered && <Button title={answer === '' ? 'Please choose an option' : ''} disabled={answer === ''}>Vote!</Button> }
+        </form>
       </div>
     :
-      <h3>Question for id {question_id} not found</h3>
+      <div>
+        <h3>Question for id {question_id || '[blank]'} not found</h3>
+        <p>Please go back or select an option from the menu</p>
+      </div>
+
     )
   }
 }
@@ -21,20 +47,16 @@ class ViewQuestion extends Component {
 const mapStateToProps = ({ authedUser, questions, users }, { match }) => {
   const question_id = match.params.question_id;
   const question = questions[question_id];
-  const gotQuestion = question_id && question;
-  const votes1 = gotQuestion ? question.optionOne.votes : []
-  const votes2 = gotQuestion ? question.optionTwo.votes : []
-  const authorName = gotQuestion ? users[question.author].name : ''
-  const hasAnswered = gotQuestion && (question.optionOne.votes.includes(authedUser) || question.optionTwo.votes.includes(authedUser))
-  console.log("ViewQuestion - question_id: ", question_id, " - question: ", question)
-
+  const votes1 = question ? question.optionOne.votes : []
+  const votes2 = question ? question.optionTwo.votes : []
+  const authorName = question ? users[question.author].name : ''
+  const hasAnswered = question && (question.optionOne.votes.includes(authedUser) || question.optionTwo.votes.includes(authedUser))
   return {
     authedUser: authedUser,
     question_id: question_id,
     question: question,
     hasAnswered: hasAnswered,
     authorName: authorName,
-    gotQuestion: gotQuestion,
     votes1: votes1,
     votes2: votes2
   }
