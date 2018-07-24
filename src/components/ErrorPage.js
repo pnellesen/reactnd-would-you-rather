9b9e132fedb9035f64694b7056444e2e6f0b6f5c
pnellesen <pnellesen@hotmail.com>
2018-07-24 09:50:41 -0500

    Minor ui improvements to error page

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,11 +1,22 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux'
 function ErrorPage(props)  {
-    return (
+  return (
       <div>
-        <h1>Error/404 component here</h1>
+        <h1>Oops! {props.location ? `"${props.location.pathname}"` : 'That page'} not available</h1>
+        {props.authedUser === null ?
+           <p>Please <Link to={'/login'}>login</Link> to use the app</p>
+          :
+           <p>Please select an option from the menu to continue</p>}
       </div>
     );
 }
 
-export default ErrorPage;
+const mapStateToProps = ({ authedUser }) => {
+	return {
+		authedUser: authedUser,
+	 }
+}
+
+export default connect(mapStateToProps)(ErrorPage);
