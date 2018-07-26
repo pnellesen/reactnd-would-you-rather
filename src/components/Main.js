45fd39e12b2a02cfbe2c845d8ad554c679c4f3d7
Pat Nellesen <pnellesen@hotmail.com>
2018-07-26 05:52:37 -0500

    Added Tabs to main page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import QuestionList from './QuestionList'
 import { connect } from 'react-redux'
+import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
 
-function Main(props) {
-  const { questions } = props;
+
+class Main extends Component {
+  state = {
+    activeTab: '1'
+  }
+
+  toggle(tab) {
+    this.state.activeTab !== tab && this.setState({activeTab: tab})
+  }
+
+  render() {
+    const { questions } = this.props;
+    console.log("active tab? ", this.state.activeTab)
     return (
-      <div>
-        {Object.keys(questions).length > 0 ? (
-        <div>
+
+        Object.keys(questions).length > 0 ? (
+          <div>
+          <Nav tabs>
+        <NavItem>
+          <NavLink
+            className={this.state.activeTab === '1' ? 'active' : '' }
+            onClick={() => { this.toggle('1'); }}
+          >
+            Unanswered
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink
+            className={this.state.activeTab === '2' ? 'active' : ''  }
+            onClick={() => { this.toggle('2'); }}
+          >
+           Answered
+          </NavLink>
+        </NavItem>
+      </Nav>
+        <TabContent activeTab={this.state.activeTab}>
+        <TabPane tabId="1">
           <h3>Questions you have not yet answered:</h3>
           <QuestionList type={'unanswered'}/>
-          <h3>Questions you have answered:</h3>
+        </TabPane>
+        <TabPane tabId="2">
+         
+        <h3>Questions you have answered:</h3>
           <QuestionList type={'answered'}/>
-        </div>
-        
-       ) : <h3>Loading questions...</h3>}
-       </div>
+        </TabPane>
+        </TabContent>
+        </div> 
+       ) : (
+       <h3>Loading questions...</h3>
+       )
+       
     );
+  }
 }
 
 const mapStateToProps = ({ questions }) => {
